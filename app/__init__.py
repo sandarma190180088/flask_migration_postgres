94c3a373dc2f8769982f3812e23c32c695664a5a
nsandarma <nsandarma@gmail.com>
2023-01-28 00:22:18 +0700

    create api and schema object (marshmallow)

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,22 @@
-from flask import Flask,jsonify,make_response
+from flask import Flask,jsonify,make_response,request
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_restful import Api,Resource
-
+from flask_marshmallow import Marshmallow
 
 
 app =  Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
-api = 
+api = Api(app=app) 
+ma = Marshmallow(app)
 
-migrate = Migrate(app,db)
+migrate = Migrate(app,db) 
 
 
 
-from app import models
-from app import routes
+from app.models import User
+from app.api import *
+from app.routes import *
 
--- app/api.py
+++ app/api.py
@@ -1,1 +1,52 @@
+from app import api,Resource,User,ma,request,db
+
+# create mapping object dari sqlalchemy
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ("username","password","created_at")
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+
+
+class User_(Resource):
+    def get(self):
+        q = User.query.all()
+        return users_schema.dump(q)
+    def post(self):
+        username = request.form.get("username")
+        password = request.form.get("password")
+        try:
+            q = User(username=username,password=password)
+            db.session.add(q)
+            db.session.commit()
+            return {'mgs':'success','status':True}
+        except Exception as e:
+            return {'msg':f'{e}','status':False}
+    def put(self):
+        args = request.args['username']
+        try:
+            q = User.query.filter_by(username=args).first()
+            q.username = request.form.get('username')
+            q.password = request.form.get('password')
+            db.session.commit()
+            return {'msg':'updated','status':True}
+        except Exception as e:
+            return {'msg':f'{e}','status':False}
+
+    def delete(self):
+        args = request.args['username']
+        try:
+            q = User.query.filter_by(username=args).first()
+            db.session.delete(q)
+            return {'msg':"deleted",'status':True}
+        except Exception as e:
+            return {'msg':f'{e}','status':False}
+
+
+
+
+
+api.add_resource(User_,'/api/user')
+
 
