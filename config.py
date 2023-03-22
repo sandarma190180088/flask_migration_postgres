0ddcb828374aa6d0b232fc072aec5e6fd65ee818
sandarma190180088 <sandarma190180088@mhs.unimal.ac.id>
2023-03-23 00:47:55 +0700

    three commit

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config(object):
     HASHIDS_SALT = 'secret!'
     JWT_SECRET_KEY = 'fasism123'
 
-    SQLALCHEMY_DATABASE_URI= f"postgres://xmzkjvkvsjowvj:bc7db9c460fb505b7805fbcadd540a106f6018ba21dee6d5e73e9723df31f96b@ec2-3-208-74-199.compute-1.amazonaws.com:5432/d2p2oee8tklnjc"
+    SQLALCHEMY_DATABASE_URI= f"postgresql://xmzkjvkvsjowvj:bc7db9c460fb505b7805fbcadd540a106f6018ba21dee6d5e73e9723df31f96b@ec2-3-208-74-199.compute-1.amazonaws.com:5432/d2p2oee8tklnjc"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_RECORD_QUERIES = True
     
